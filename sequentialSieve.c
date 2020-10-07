b472585431829f57722fc6fcd31945e7dce550bc
Erikct98 <e.c.takke@student.tue.nl>
2020-10-07 11:07:01 +0200

    Small fix to sequentialSieve - Basic does not report 1 as prime anymore. - Advanced printing more in line with other algorithms. - End time determined before printing primes. More in line with other timings.

--- sequentialSieve.c
+++ sequentialSieve.c
@@ -6,15 +6,15 @@
  * Reports all primes in the range [2,S)
  * No fancy trickery
  */
-void sequentialSieveBasic(){
+void sequentialSieveBasic() {
     clock_t startTime, endTime;
 
     // Start timing
     startTime = clock();
 
     // Setup sieve
-    bool* sieve = malloc(S * sizeof(bool));
-    for (int i = 0; i < S; i++){
+    bool *sieve = malloc(S * sizeof(bool));
+    for (int i = 0; i < S; i++) {
         sieve[i] = true;
     }
 
@@ -22,17 +22,20 @@ void sequentialSieveBasic(){
     long fqrtS = intSqrt(S, true);
 
     // Run sieve
-    for (long i = 2; i <= fqrtS; i++){
-        if (sieve[i]){
-            for (long j = i * i; j < S; j += i){
+    for (long i = 2; i <= fqrtS; i++) {
+        if (sieve[i]) {
+            for (long j = i * i; j < S; j += i) {
                 sieve[j] = false;
             }
         }
     }
 
+    // Determine end running time
+    endTime = clock();
+
     // Report primes
-    for (long i = 1; i < S; i++){
-        if (sieve[i]){
+    for (long i = 2; i < S; i++) {
+        if (sieve[i]) {
             printf("%ld,", i);
         }
     }
@@ -41,9 +44,8 @@ void sequentialSieveBasic(){
     free(sieve);
 
     // Report running time
-    endTime = clock();
     clock_t time_taken = (endTime - startTime);
-    printf("\nsequentialSieveBasic time: %ld to %ld (%f)\n", startTime, endTime, (double)time_taken / CLOCKS_PER_SEC);
+    printf("\nsequentialSieveBasic time: %ld to %ld (%f)\n", startTime, endTime, (double) time_taken / CLOCKS_PER_SEC);
 }
 
 /*
@@ -51,35 +53,38 @@ void sequentialSieveBasic(){
  * Reports all primes in the range [2,S)
  * Does not take multiples of two into account
  */
-void sequentialSieveAdvanced(){
+void sequentialSieveAdvanced() {
     clock_t startTime, endTime;
 
     // Start timing
     startTime = clock();
 
     // Setup sieve
-    bool *sieve = malloc(S/2 * sizeof(bool));
-    for (int i = 0; i < S/2; i++){
+    bool *sieve = malloc(S / 2 * sizeof(bool));
+    for (int i = 0; i < S / 2; i++) {
         sieve[i] = true;
     }
 
     // Compute upper bound
-    long fqrtS = (intSqrt(S, true) - 1)/2;
+    long fqrtS = (intSqrt(S, true) - 1) / 2;
 
     // Run sieve
-    for (long i = 1; i <= fqrtS; i++){
-        if (sieve[i]){
+    for (long i = 1; i <= fqrtS; i++) {
+        if (sieve[i]) {
             long p = 2 * i + 1;
-            for (long j = i * (p+1); j < S/2; j += p){
+            for (long j = i * (p + 1); j < S / 2; j += p) {
                 sieve[j] = false;
             }
         }
     }
 
+    // Determine end running time
+    endTime = clock();
+
     // Report primes
-    printf("Found a prime: %ld\n", 2l);
-    for (long i = 1; i < S/2; i++){
-        if (sieve[i]){
+    printf("%ld,", 2l);
+    for (long i = 1; i < S / 2; i++) {
+        if (sieve[i]) {
             long p = 2 * i + 1;
             printf("%ld,", p);
         }
@@ -89,9 +94,8 @@ void sequentialSieveAdvanced(){
     free(sieve);
 
     // Report running time
-    endTime = clock();
     clock_t time_taken = (endTime - startTime);
-    printf("\nsequentialSieveBasic time: %ld to %ld (%f)\n", startTime, endTime, (double)time_taken / CLOCKS_PER_SEC);
+    printf("\nsequentialSieveBasic time: %ld to %ld (%f)\n", startTime, endTime, (double) time_taken / CLOCKS_PER_SEC);
 }
 
 int main(int argc, char **argv) {
@@ -106,9 +110,9 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Version %u does not exist.\n", decide);
     }
 
-    if (decide == 0){
+    if (decide == 0) {
         sequentialSieveBasic();
-    } else if (decide == 1){
+    } else if (decide == 1) {
         sequentialSieveAdvanced();
     }
 
