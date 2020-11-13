9287c22e9cdc6abc4a95505a2541ed11a0ee04a2
davidot <david.tuin@gmail.com>
2020-11-13 10:18:24 +0100

    Add basic drawing of chess board

--- chess.h
+++ chess.h
@@ -1,6 +1,29 @@
 #ifndef PAF20_CHESS_H
 #define PAF20_CHESS_H
 
+#include <stdio.h>
+
+
+void printBoard(int N, int* board) {
+    for (int i = N - 1; i >= 0; --i) {
+        printf("%2x ", i);
+        for (int j = 0; j < N; ++j) {
+            if (board[j] == i) {
+//                printf("♕");
+                printf("#");
+            } else {
+                printf(" ");
+            }
+//            printf(" %*s#\n", i + 1, board[i], "");
+        }
+        printf("\n");
+    }
+    printf(" - ");
+    for (int i = 0; i < N; ++i) {
+        printf("%x", i % 16);
+    }
+    printf("\n");
+}
 
 
 #endif //PAF20_CHESS_H
--- recursive.c
+++ recursive.c
@@ -1,7 +1,8 @@
+#include "chess.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#include "chess.h"
 int N;
 
 void recursiveSolve() {
