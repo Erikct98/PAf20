985221514c8e6c5454a4426b53233a8febaae2ec
Erikct98 <e.c.takke@student.tue.nl>
2020-11-13 10:19:25 +0100

    Add recursive function

--- chess.h
+++ chess.h
@@ -1,6 +1,8 @@
 #ifndef PAF20_CHESS_H
 #define PAF20_CHESS_H
 
+#include "edupack/bspedupack.h"
+
 
 
 #endif //PAF20_CHESS_H
--- recursive.c
+++ recursive.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include "chess.h"
-int N;
+static const int N = 8;
+
+void interference(int *board, int idx) {
+    int idxHeight = board[idx];
+    for (int i = 0; i < idx; i++){
+        int horDiff = idx - i;
+        int verDiff = abs(idxHeight - board[i]);
+        if (horrDiff == verDiff) return true;
+    }
+    return false;
+}
+
+bool recursiveCall(int *board, bool *used, int idx) {
+    for (int i = 0; i < N; i++) {
+        if (!used[i]) {
+            board[idx] = i;
+            used[i] = true;
+            if (!interference(board, idx) && recursiveCall(board, used, idx + 1)) {
+                return true;
+            }
+            used[i] = false;
+        }
+    }
+    return false;
+}
 
 void recursiveSolve() {
     clock_t startTime, endTime;
@@ -10,9 +34,24 @@ void recursiveSolve() {
     // Start timing
     startTime = clock();
 
+    // Setup sieve
+    int *board = malloc(N * sizeof(int));
+    int *used = malloc(N * sizeof(bool));
+    for (int i = 0; i < N; i++) sieve[i] = -1;
 
+    // Start recursive call
+    bool foundSolution = recursive(board, 0);
 
+    // End timing
     endTime = clock();
+
+    // Print result
+    if (!foundSolution) {
+        printf("Failed to find a solution!");
+    } else {
+        // TODO: print solution.
+    }
+
     // Report running time
     clock_t time_taken = (endTime - startTime);
     printf("Solving %d-queens took recursiveSolve: %f\n", N, (double) time_taken / CLOCKS_PER_SEC);
