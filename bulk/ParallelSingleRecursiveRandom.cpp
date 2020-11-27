9271a9f55d4811da3c2db637b521326f58d30c8a
Erikct98 <e.c.takke@student.tue.nl>
2020-11-27 16:05:10 +0100

    Attempt to implement "BasicParallel"

---
+++ bulk/ParallelSingleRecursiveRandom.cpp
@@ -0,0 +1,72 @@
+#include <bulk/backends/thread/environment.hpp>
+#include <deque>
+#include <iostream>
+#include "chess.h"
+#include "DiagonalBitSet.h"
+
+#define FOUND -1
+#define STOPPING -2
+#define SYNC_THRESHOLD 2000000
+
+class ParallelSingleRecursiveRandom {
+private:
+    uint32_t findSolution(bulk::world &world, const Board &board, uint32_t idx, DiagonalBitSet &diag, bulk::var<bool> *done) {
+        if (idx == N) {
+            auto p = world.active_processors();
+            for (int j = 0; j < p; j++) bulk::put((world.rank() + j) % p, true, *done);
+            return FOUND;
+        }
+
+        uint32_t count = 0;
+        for (uint32_t i = idx; i < N; i++) {
+            count++;
+            if (!diag.hasInterference(idx, board[i])) {
+                diag.set(idx, board[i]);
+                uint32_t res = findSolution(world, board, idx + 1, diag, done);
+                if (res == FOUND || res == STOPPING) return res;
+                count += res;
+                diag.reset(idx, board[i]);
+            }
+
+            if (count > SYNC_THRESHOLD) {
+                count = 0;
+                world.sync();
+                if (*done) return STOPPING;
+            }
+        }
+        return count;
+    }
+
+public:
+    std::optional<Board> solve() {
+        bulk::thread::environment env;
+        env.spawn(P, [](bulk::world &world) {
+            auto s = world.rank();
+            auto p = world.active_processors();
+
+            // Initialize board
+            Board board{N};
+            for (uint32_t j = 0; j < N; j++) board[j] = j;
+            // TODO: shuffle
+
+            // Initialize diagonal
+            DiagonalBitSet diag{N};
+
+            // Initialize found flag
+            auto done = bulk::var<bool>(world);
+            done = false;
+            world.sync();
+
+            // Find solution
+            auto res = findSolution(world, board, 0, diag, &done);
+            if (res == FOUND) {
+                std::cout << "(";
+                for (auto v : board) {
+                    std::cout << v << ", ";
+                }
+                std::cout << ')\n';
+                std::cout << "By " << s << " / " << p << '\n';
+            }
+        });
+    }
+};
---
+++ bulk/ParallelSingleRecursiveRandom.h
@@ -0,0 +1,20 @@
+#ifndef PAF20_PARALLELALGORITHMS_PARALLELSINGLERECURSIVERANDOM_H
+#define PAF20_PARALLELALGORITHMS_PARALLELSINGLERECURSIVERANDOM_H
+
+#include <bulk/backends/thread/environment.hpp>
+#include <deque>
+#include <iostream>
+#include "chess.h"
+
+class ParallelSingleRecursiveRandom {
+private:
+    uint32_t N;
+    uint32_t P;
+
+    uint32_t findSolution(bulk::world &world, const Board &board, uint32_t idx, DiagonalBitSet &diag, bulk::var<bool> *done);
+
+public:
+    void solve();
+};
+
+#endif //PAF20_PARALLELALGORITHMS_PARALLELSINGLERECURSIVERANDOM_H
