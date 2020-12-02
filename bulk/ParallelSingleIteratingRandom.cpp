ef3327ae4e9174804184838602828d206e416f53
Erikct98 <e.c.takke@student.tue.nl>
2020-12-02 11:27:49 +0100

    Merge board and remaining for PSIteratingRandom

--- bulk/ParallelSingleIteratingRandom.cpp
+++ bulk/ParallelSingleIteratingRandom.cpp
@@ -1,51 +1,54 @@
 #include "ParallelSingleIteratingRandom.h"
 #include "DiagonalBitSet.h"
 
-std::optional<Board> ParallelSingleIteratingRandom::findSolution(const Board& partial, RemainingList remaining) {
-    DiagonalBitSet diag {N};
+std::optional<Board> ParallelSingleIteratingRandom::findSolution(const Board &partial, RemainingList remaining) {
+    // Check sizes
     const uint32_t upto = partial.size();
-
-    Board board (N, initValue);
-
-    if (N - upto != remaining.size()) {
+    if (remaining.size() + upto != N) {
         std::cout << "Failure!\n";
         return {};
     }
 
+    // Setup board
+    Board board(N);
     for (uint32_t i = 0; i < upto; ++i) {
         board[i] = partial[i];
-        diag.set(i, partial[i]);
+    }
+    for (uint32_t i = 0; i < remaining.size(); ++i) {
+        board[upto + i] = remaining[i];
     }
 
+    // Setup diagonals
+    DiagonalBitSet diag{N};
+    for (uint32_t i = 0; i < upto; ++i) {
+        diag.set(i, board[i]);
+    }
+
+    // Setup picked state
     std::vector<uint32_t> picks;
     picks.reserve(remaining.size());
 
     uint32_t currIdx = upto;
-    uint32_t remainingIdx = 0;
-    auto remainingBack = remaining.size();
+    uint32_t remainingIdx;
     while (currIdx < N) {
-        while (remainingIdx < remainingBack && diag.hasInterference(currIdx, remaining[remainingIdx])) {
+        remainingIdx = currIdx;
+        while (remainingIdx < N && diag.hasInterference(currIdx, board[remainingIdx])) {
             ++remainingIdx;
         }
-        if (remainingIdx >= remainingBack) {
+        if (remainingIdx >= N) {
             currIdx--;
             if (currIdx < upto) {
                 return {};
             }
             remainingIdx = picks.back();
             picks.pop_back();
-            remaining[remainingIdx] = board[currIdx];
             diag.reset(currIdx, board[currIdx]);
-            remainingIdx++;
-            remainingBack++;
+            swap(&board[currIdx], &board[remainingIdx]);
         } else {
-            board[currIdx] = remaining[remainingIdx];
+            swap(&board[currIdx], &board[remainingIdx]);
             diag.set(currIdx, board[currIdx]);
             picks.push_back(remainingIdx);
-            remaining[remainingIdx] = remaining[remainingBack - 1];
-            --remainingBack;
-            remainingIdx = 0;
-            ++currIdx;
+            currIdx++;
         }
     }
 
@@ -54,15 +57,19 @@ std::optional<Board> ParallelSingleIteratingRandom::findSolution(const Board& pa
 
 void ParallelSingleIteratingRandom::solve() {
     bulk::thread::environment env;
-    env.spawn(P, [&](bulk::world& world) {
+    env.spawn(P, [&](bulk::world &world) {
         auto s = world.rank();
         auto p = world.active_processors();
 
-        auto hasSolution = findSolution({}, {0, 1, 2, 3, 4, 5, 6, 7});
+        RemainingList range(N);
+        for (uint32_t i = 0; i < N; i++) range[i] = i;
+        shuffle(range);
+
+        auto hasSolution = findSolution({}, range);
         if (hasSolution) {
             auto solution = *hasSolution;
             for (auto v : solution) {
-                std:: cout << v << ", ";
+                std::cout << v << ", ";
             }
             std::cout << '\n';
             std::cout << "By " << s << " / " << p << '\n';
--- bulk/chess.h
+++ bulk/chess.h
@@ -3,5 +3,22 @@
 
 using Board = std::vector<uint32_t>;
 
+void swap(uint32_t *one, uint32_t *two) {
+    uint32_t temp = *one;
+    *one = *two;
+    *two = temp;
+}
+
+void shuffle(std::vector<uint32_t> array) {
+    size_t n = array.size();
+    if (size > 1) {
+        size_t i;
+        for (i = 0; i < n - 1; i++) {
+            size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+            swap(&array[i], &array[j]);
+        }
+    }
+}
+
 
 #endif //PAF20_PARALLELALGORITHMS_CHESS_H
