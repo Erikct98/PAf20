3c82fddac7ca033059ffa974fb6ca2606cc28d16
Erikct98 <e.c.takke@student.tue.nl>
2020-12-05 13:46:07 +0100

    CountIterating. - Work distribution incorporated.

--- bulk/CountIterating.cpp
+++ bulk/CountIterating.cpp
@@ -5,8 +5,9 @@ CountIterating::CountIterating(uint32_t n, uint32_t procs) : N(n), procs(procs) 
 
 uint32_t CountIterating::countSolutions(Board &board) const {
     uint32_t count = 0;
+    uint64_t endIdx = board.getIdx() - 1; // Dirty hack, but it works
 
-    while (board.getIdx() < 0xffffffff) {
+    while (board.getIdx() != endIdx) {
         if (board.getIdx() == N) {
             count += 1;
             board.pop();
@@ -32,14 +33,54 @@ void CountIterating::solve() {
         // Compute number of cases
         uint32_t depth = 0, cases = 1;
         while (p * p > cases) {
-            cases *= N;
+            cases *= N - depth;
             depth++;
         }
 
         // Init board
         Board board(N);
-        uint32_t count = countSolutions(board);
-        std::cout << "count: " << count << std::endl;
+
+        // Count all solutions
+        uint32_t count = 0;
+        for (uint32_t caseNr = s; caseNr < cases; caseNr += p) {
+            // Reset board
+            board.reset();
+
+            // Create placeholder
+            std::vector<uint64_t> placeHolder(N);
+            for (uint32_t i = 0; i < N; i++) placeHolder[i] = i;
+
+            // Setup case
+            uint32_t i = caseNr, j, idx;
+            for (j = 0; j < depth; j++) {
+                idx = i % (N - j);
+                i /= (N - j);
+
+                uint64_t available = board.getAvailable();
+                if ((available & (1u << placeHolder[j + idx])) == 0) break;
+                board.push(1u << placeHolder[j + idx]);
+                std::swap(placeHolder[j], placeHolder[j + idx]);
+            }
+            if (j != depth) continue; // Diagonal clash -> go to next case
+
+            // Count number of solutions
+            count += countSolutions(board);
+        }
+
+        // Exchange counts
+        auto countExchange = bulk::coarray<uint32_t>(world, p);
+        for (uint32_t k = 0; k < p; k++) {
+            countExchange(k)[s] = count;
+        }
+        world.sync();
+        uint32_t totalCount = 0;
+        for (auto c : countExchange) {
+            totalCount += c;
+        }
+
+        if (s == 0) {
+            std::cout << "Total count: " << totalCount << std::endl;
+        }
     });
 }
 
